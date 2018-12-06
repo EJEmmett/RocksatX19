5f7516930d3afedec77c8adbca67c08bc39d8c50
sschatz1997 <s.schatz1997@gmail.com>
2018-12-06 10:30:50 -0500

    added stuff

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//this is the main program for the networking side of this years rocksat!
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+
+
+
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
