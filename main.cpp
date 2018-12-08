54bca8322d408ded662c6c9cd4f17bd1c1ed36ff
sschatz1997 <s.schatz1997@gmail.com>
2018-12-08 13:31:44 -0500

    added time

--- main.cpp
+++ main.cpp
@@ -4,16 +4,25 @@
 #include <fstream>
 #include <sys/types.h>
 #include <winsock2.h>
+#include <time.h>
 //#include <netdb.h>
 
 #include "hash.h"
 using namespace std;
+
+int getTime()
+{
+	int c;
+	c = clock();
+	return c;
+}
 
 int main(int argc, char** argv) {
 	Hash h;
 	string hash;
 //	h.setValue("test");
 //	h.outPut();
+	cout << "time: " << getTime();
 	hash = h.getValue();
 	system("PAUSE");
 	return 0;
