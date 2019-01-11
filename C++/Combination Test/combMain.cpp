9920a763effa7c611470744f3bd6243621a4f83a
sschatz1997 <s.schatz1997@gmail.com>
2019-01-11 13:59:08 -0500

    added the testing menu

--- C++/Combination Test/combMain.cpp
+++ C++/Combination Test/combMain.cpp
@@ -8,6 +8,18 @@
 #include "writing.h"
 using namespace std; using std::cout; using std::endl;
 
+int menu()
+{
+	int choice;
+	cout<<"\t\tCTU RockSat2019 Testing:\n"
+		<<"\t1. Run MD5 Generator.\n"
+		<<"\t2. Print Time.\n"
+		<<"\t9. Exit.\n";
+	cin >> choice;
+//the menu will never be cleared while this is tested. system("CLS");
+	return choice;
+}
+
 int getTime()
 {
 	int c;
@@ -15,20 +27,36 @@ int getTime()
 	return c;
 }
 
-
 int main(int argc, char** argv) {
 	Hash h;
 	Writing write;
 	string hash;
-//	int time;
+	int time, input;
 //	time = getTime();
-	h.setValue("ssdd");
-	h.outPut();
-	hash = h.getValue();
+	do
+	{
+		input = menu();
+		switch(input)
+		{
+			case 1:
+				h.setValue("ssdd");
+				h.outPut();
+				hash = h.getValue();
+				write.mainWrite();
+			break;
+			case 2:
+				cout << "time: " << getTime();
+			break;
+			case 9: cout <<"EXITING"<<endl;
+			break;
+			default: cout << "invalid choice"<<endl;
+			break;
+		}
+	}while(input != 9);
 //	cout << "\n MD5: " << hash;
 	//system("PAUSE");
 	//Print funciton for both ints and strings
-	write.mainWrite();
+	
 
 	return 0;
 }
