127af0ba147dde313c9e01d0bd9819a4ddd38f68
sschatz1997 <s.schatz1997@gmail.com>
2019-01-13 13:29:23 -0500

    string comb testing completion

---
+++ C++/testing/String combination testing/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	//this is to test if we can combine strings on the fly.
+	string s1, s2, s3, s4;
+	s1 = "string1";
+	s2 = "string2";
+	s3 = "string3";
+	s4 = s1 + s2 + s3;
+	cout << s4 << endl;
+	
+	return 0;
+}
