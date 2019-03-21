0270b4addc0d15b0e61e668f1ed924e984ac0211
sschatz1997 <s.schatz1997@gmail.com>
2019-03-21 15:27:34 -0400

    more testing

---
+++ C++/testing/Redesign MD5/main.cpp
@@ -0,0 +1,71 @@
+//main.cpp
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+#include <vector>
+#include <cstdlib>
+#include <unistd.h>
+#include <ctime>
+#include "wordGenNew.h"
+#include "wordGenNew.cpp"
+#include "MD5New.h"
+#include "MD5New.cpp'
+
+using namespace std;
+
+int menu(){
+	int choice;
+	cout<<"CTU RockSat2019 Testing:\n\n"
+		<<"1. Run MD5 Generator.\n"
+		<<"2. Print Time.\n"
+		<<"3. Random Number test\n"
+		<<"4. Random number loop testing\n"
+		<<"5. Read file list test\n"
+		<<"6. Exit.\n";
+	cin >> choice;
+//the menu will never be cleared while this is tested. system("CLS");
+	return choice;
+}
+
+char* getTime(){
+	return ctime(new time_t(time(0)));
+	//COMMENT: gets the time from the system which will only be used for testing.
+}
+
+
+int main(){
+  Hash h;
+  string hash;
+  int input, r;
+
+  do{
+    input = menu();
+    switch(input){
+      case 1:
+        //COMMENT: randomly sets the hash value, currently a work in progress.
+        h.setValue("ssdd");
+        h.outPut();
+        hash = h.getValue();
+        write.mainWrite();
+      break;
+      case 2:
+        cout << "time: " << getTime();
+      break;
+      case 3:
+        cout << "Random number: " << rand.returnNumber() << endl;
+      break;
+      case 4:
+        cout << "Random number loop testing\n" << rand.getWordComb() << endl;
+      break;
+      case 5:
+        //COMMENT: reads the random word
+        rand.readFileTest();
+      break;
+      case 6: cout << "exiting"<<endl;
+			default: cout << "invalid choice"<<endl;
+			break;
+  }while(input != 6);
+  return 0;
+}
---
+++ C++/testing/Redesign MD5/wordGenNew.cpp
@@ -0,0 +1,75 @@
+//wordGenNew.cpp
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include "randomWordGen.h"
+using namespace std;
+
+Random::Random()
+{
+
+}
+
+void Random::setRandom(int r)
+{
+	random = r;
+}
+
+int Random::returnNumber()
+{
+	int r;
+	r = rand() %  80277 + 1;
+	return r;
+}
+
+int Random::getWordComb()
+{
+	int i, r, r1, r2;
+	i = 0;
+	do{
+		r1 = Random::returnNumber();
+		r2 = r1 + r2;
+		i = i + 1;
+	}while(i < 10);
+	return r2;
+}
+
+void Random::readFileTest()
+{
+	ifstream in("dictionary.txt");
+	string line, s, word, mainWords;
+	int rand, rand2, count;
+	double tmp;
+	//COMMENT: this is a test to get the line number of a word.
+	/*tmp == temp var
+	while (getline(in, line))
+	{
+		cout << line <<endl;
+	}*/
+	rand = Random::returnNumber();
+	//cout << rand << endl;
+	ifstream file1 ("dictionary.txt", ios::in);
+
+	vector<string> readFromFile;
+
+	while(file1>>s)
+	{
+		readFromFile.push_back(s);
+	}
+
+	/*for(int i=0; i<readFromFile.size(); i++)
+	{
+		cout<<readFromFile[i]<<endl;
+	}*/
+	//cout << readFromFile[rand-1] << endl;
+	for(int o=0; o<5; o++)
+	{
+		//COMMENT: random word from the dictonary test.
+		rand2 = Random::returnNumber();
+		word = readFromFile[rand2-1];
+		mainWords = mainWords + " " + word;
+	}
+	cout << mainWords;
+}
